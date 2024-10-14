354be8f5a746e12edb67dbbd14a5dcaf3986ebab
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-10-14 00:01:05 -0300

    updating static flags

--- actions.py
+++ actions.py
@@ -2,33 +2,40 @@ import pyautogui as pg
 import constants
 
 def battle_empty():
-    return pg.locateOnScreen('imgs\\empty-battle.png', region=constants.BATTLE_REGION)
+    return pg.locateOnScreen('imgs\\empty-battle.png', region=constants.BATTLE_REGION, confidence=0.8)
 
 def attacking_monster():
-    return pg.locateOnScreen('imgs\\red-target-attacking.png', region=constants.BATTLE_REGION, confidence=0.6) or pg.locateOnScreen('imgs\\red-target-defending.png', region=constants.BATTLE_REGION, confidence=0.8)
-
-def kill_monster():
-    while battle_empty == None:
+    return pg.locateOnScreen('imgs\\red-target.png', region=constants.BATTLE_REGION, confidence=0.92) 
+    # or pg.locateOnScreen('imgs\\red-black-target.png', region=constants.BATTLE_REGION, confidence=0.72)
+  
+def kill_monster(): 
+    while battle_empty() == None:
         pg.press('space')
-        print('monstro alvejado...') 
-        while attacking_monster(): 
-            print('esperando monstro morrer...') 
+        print('monstro alvejado.')                           
+        while attacking_monster() != None: 
+            pg.sleep(1)
+            print('esperando monstro morrer...')
+        print('procurando outro monstro...')
 
 def go_to_flag(path, wait):
     flag = pg.locateOnScreen(path, region=constants.MINI_MAP_REGION, confidence=0.8)
-    x, y = pg.center(flag)
-    pg.moveTo(x, y)
-    pg.click(wait)
+    print('going to... ', path)
+    if flag:
+        x, y = pg.center(flag)
+        pg.moveTo(x, y)
+        pg.click()
+        pg.sleep(wait)
 
 def check_player_position():
     return pg.locateOnScreen('imgs\\point-player.png', region=constants.MINI_MAP_REGION, confidence=0.8)
 
 def get_loot():
-    loot = pg.locateOnScreen('imgs\\dead-wasp.png', region=constants.LOOT_REGION, confidence=0.7)
-    if loot:
-        x, y = pg.center(loot)
-        pg.moveTo(x, y)
-        pg.click(button="right")
+    # loot = pg.locateOnScreen('imgs\\dead-wasp.png', region=constants.LOOT_REGION, confidence=0.7)
+    # if loot:
+    #     x, y = pg.center(loot)
+    #     pg.moveTo(x, y)
+    #     pg.click(button="right")
+    pg.hotkey("alt","q")
 
 def check_status(name, delay, position, rgb, button):
     print(f'checando {name}...')
--- main.py
+++ main.py
@@ -16,10 +16,12 @@ def run():
         coordinates = json.loads(file.read())
     for coordinate in coordinates:
         actions.kill_monster()
+        pg.sleep(1)
         actions.get_loot()
         actions.go_to_flag(coordinate['path'], coordinate['wait'])
         if actions.check_player_position:
             actions.kill_monster()
+            pg.sleep(1)
             actions.get_loot()
             actions.go_to_flag(coordinate['path'], coordinate['wait'])
         actions.eat_food()
@@ -27,7 +29,23 @@ def run():
         actions.up_hole(coordinate['up_hole'], f'{constants.FOLDER_NAME}\\anchor-floor-02.png', 420, 0)
         actions.up_hole(coordinate['up_hole'], f'{constants.FOLDER_NAME}\\anchor-floor-03.png', 147, 120)
 
-run()
+             
+# keyboard.wait('h')
+# run()
+
+while True:
+    keyboard.wait('h')
+    box = actions.attacking_monster()
+    if box:
+        print(f'Imagem encontrada na região: {box}')  # Exibe a coordenada e o tamanho da box encontrada
+        # Converta os valores da box em inteiros simples
+        box_int = (int(box.left), int(box.top), int(box.width), int(box.height))
+        # Capturar a screenshot da região que a imagem foi encontrada
+        screenshot = pg.screenshot(region=box_int)
+        screenshot.save('captura_monstro.png')
+        print('Screenshot salva como captura_monstro.png')
+    else:
+        print('Imagem não encontrada')
 
 
 
